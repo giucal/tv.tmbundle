012fb2af6059a874cdb539a778e3cada1ca8c582
Giuseppe Calabrese <ugiucal@gmail.com>
2019-05-07 01:13:18 +0200

    Initial commit

---
+++ Support/common.rb
@@ -0,0 +1,54 @@
+# Needed by individual commands.
+require "#{ENV['TM_SUPPORT_PATH']}/lib/exit_codes"
+
+# An entry as found in a "TV" file.
+#
+# An entry represents the state of a TV programme, described by the
+# following attributes.
+#
+#   - Title.   The name of the programme.
+#   - Season.  The season being currently in the watching.
+#   - Episode. The last watched episode of the season.
+#   - Comment. An addendum.
+#
+# An entry is expected to comply with the format
+#
+#   <season> "x" <episode> " "* "|" " "* <title> [" "* "--" <comment>]
+#
+class Entry < Struct.new(:title, :season, :episode, :comment)
+  # Entry format.
+  RE = /^(\d+)x(\d+)[ \t]*\|[ \t]*\b(.+)\b([ \t]*(?:--.*)?)$/
+
+  # Comment comprehensive of leading space and "--" mark.
+  # Useful to maintain the alignment of comments.
+  attr_reader :trailing
+
+  private
+  def initialize(title, season, episode, trailing)
+    @trailing = trailing
+    super(title, season, episode, trailing.strip)
+  end
+
+  # Parses an entry.
+  #
+  # Fails with an ArgumentError if the line is malformed.
+  # This is not a truly exceptional sistuation, since the file may contain
+  # other content.
+  def self.parse(line)
+    unless line =~ RE
+      raise ArgumentError, "not a valid entry"
+    end
+
+    entry = Entry.new(
+      title = $3,
+      season = $1.to_i,
+      episode = $2.to_i,
+      trailing = $4
+    )
+  end
+
+  def to_s
+    s = format "%02dx%02d | %s", season, episode, title
+    s += trailing unless trailing.nil?
+  end
+end
